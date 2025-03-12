2ac0fb8d0dcaeecb0833b8f2d35acc838541d1bb
lunledev <183235731+lunledev@users.noreply.github.com>
2025-03-11 18:29:47 -0700

    added cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,2 +1,21 @@
 //runs the application using imports from lib/
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+    specPattern: "cypress/component/**/*.cy.{js,ts,jsx,tsx}",
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: "http://localhost:3000",
+  },
+});
+
 
